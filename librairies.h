ea59c0bb71c9f9da56292d34ada47cdce4c48a38
COLIN tim <32955194+COLINtim@users.noreply.github.com>
2017-10-25 13:38:31 +0200

    Update librairies.h

--- librairies.h
+++ librairies.h
@@ -6,9 +6,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
+
 #define couleur(param) printf("\033[%sm",param)
-#define placement(a,b) printf("\33[%d;%dHâˆ¼",a,b)
-#define placement1(a,b) printf("\33[%d;%dHâˆ½",a,b)
+
 #define nombre_de_lignes 81
 #define nombre_de_colonnes 194
 
